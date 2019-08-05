518449d906a351180545495598d6f32c3e4e9b1e
Daniel Young <daniel.young@bluefrontier.co.uk>
2019-08-05 17:38:25 +0100

    feat: posting messages from HTTP requests

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hello, World!");
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const { loggerMiddleware } = require("./logger");
+const { listenPort } = require("./config");
+const handler = require("./handler");
+
+const app = express();
+
+app.use(fileUpload());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(loggerMiddleware);
+
+app.get("/*", handler);
+app.post("/*", handler);
+
+app.listen(listenPort, () => console.log(`Listening on port ${listenPort}!`));
