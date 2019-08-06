f5397865fb5a87a4a0352839adcb2869f3a65929
Daniel Young <daniel.young@bluefrontier.co.uk>
2019-08-06 16:08:51 +0100

    feat: adding xml, csv, and plain text handlers

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const fileUpload = require("express-fileupload");
+const expressCsv = require('express-csv-middleware');
+const expressXml = require('express-xml-bodyparser');
+const textMiddleware = require("./middleware");
+const handler = require("./handler");
+
 const { loggerMiddleware } = require("./logger");
 const { listenPort } = require("./config");
-const handler = require("./handler");
 
 const app = express();
 
 app.use(fileUpload());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(expressCsv());
+app.use(expressXml());
+app.use(textMiddleware);
 app.use(loggerMiddleware);
 
 app.get("/*", handler);
