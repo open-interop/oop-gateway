bad839d9699c04a7f183d240045f63d661fc6913
Daniel Young <daniel.young@bluefrontier.co.uk>
2019-08-23 16:40:27 +0100

    fix: renaming env vars

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-    listenPort: process.env.LISTEN_PORT,
-    amqpAddress: process.env.AMQP_ADDRESS,
-    exchangeName: process.env.EXCHANGE_NAME,
-    rawMessageQ: process.env.RAW_MESSAGE_Q,
-    dbAddress: process.env.DB_ADDRESS
+    listenPort: process.env.OOP_LISTEN_PORT,
+    amqpAddress: process.env.OOP_AMQP_ADDRESS,
+    exchangeName: process.env.OOP_EXCHANGE_NAME,
+    rawMessageQ: process.env.OOP_NOAUTH_RAW_MESSAGE_Q,
+    dbAddress: process.env.OOP_DB_ADDRESS
 };
